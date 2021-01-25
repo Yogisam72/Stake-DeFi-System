779bc493011801f2581d691dd390a3449b923e34
Yogesh Pandey <Yogisam72@users.noreply.github.com>
2021-01-25 18:52:06 +0530

    Making MDT Token basics

---
+++ contracts/MDT_Token.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.17 <0.8.0;
+
+import "@openzeppelin\contracts\token\ERC20.sol";
+import "@openzeppelin\contracts\token\ERC20Capped.sol";
+
+contract MDT_Token is ERC20, ERC20Capped{
+
+	uint256 public initialSupply = 10*(10**24);
+
+	constructor() ERC20("MindDeft Token", "MDT") ERC20Capped(15*(10**24)) public(){
+		_mint(msg.sender, initialSupply);
+	}
+}
